2a002d987cef64858e39bd79bb1b56dc850a954f
jesseerrico <jesse.errico@gmail.com>
2023-04-28 15:49:24 -0400

    Initial checkin (base React app)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Content will go here.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
