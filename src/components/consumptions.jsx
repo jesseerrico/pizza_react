3a6d2c57fad8e867a1d0d616bdee1616ccfc31d4
jesseerrico <jesse.errico@gmail.com>
2023-04-28 18:36:08 -0400

    Added ability to add new consumption

--- src/components/consumptions.jsx
+++ src/components/consumptions.jsx
@@ -13,6 +13,8 @@ const Consumptions = (props) => {
 
     const [consumptions, setConsumptions] = useState([]);
     const [chosenPerson, setChosenPerson] = useState("");
+    const [pizzaType, setPizzaType] = useState("");
+    const [date, setDate] = useState("");
 
     const consumptionsUrl = "http://localhost:9292/delivery"
 
@@ -30,6 +32,34 @@ const Consumptions = (props) => {
         }
     }
 
+    // I stole this code from the Internet. No apologies.
+    const isValidDate = (dateString) => {
+        var regEx = /^\d{4}-\d{2}-\d{2}$/;
+        if(!dateString.match(regEx)) return false;  // Invalid format
+        var d = new Date(dateString);
+        var dNum = d.getTime();
+        if(!dNum && dNum !== 0) return false; // NaN value, Invalid date
+        return d.toISOString().slice(0,10) === dateString;
+      }
+      
+    // Pizza type is kept as pure text input rather than dropdown so that users can input a new one like in the API call
+    const addConsumption = () => {
+        if(chosenPerson && pizzaType && pizzaType !== "" && date && isValidDate(date)) {
+            axios.post(consumptionsUrl, {
+                person_name: chosenPerson,
+                pizza_name: pizzaType,
+                date: date
+            }).then((response) => {
+                // Refetch consumptions to display the new one
+                getConsumptions();
+            }).catch(error => {
+                console.error("Error: " + error);
+            })
+        } else {
+            alert("Error! Make sure you have chosen a person and pizza type and entered a valid date.")
+        }
+    }
+
     useEffect(() => {
         setChosenPerson(props.people[0]);
     }, [])
@@ -43,11 +73,17 @@ const Consumptions = (props) => {
         <div className="lineItem">
             <h1>Consumptions</h1>
             <select value={chosenPerson} onChange={e => handleChange(e.target.value)}>
-                <option>Choose a person to see consumptions</option>
+                <option>Choose a person</option>
                 {props.people.map(person => <option value={person}>{person}</option>)}
             </select>
             <button onClick={getConsumptions}>Find person's pizza consumptions</button>
             {consumptions.map(cons => <div className='lineItem'>{chosenPerson} ate a {cons['meat-type']} pizza on {cons['date']}</div>)}
+            <h2>Add consumption</h2>
+            <input type="text" id="pizzaInput" value={pizzaType} onChange={e => setPizzaType(e.target.value)} />
+            <label for="pizzaInput">Pizza Type</label><br/>
+            <input type="text" id="dateInput" value={date} onChange={e => setDate(e.target.value)} />
+            <label for="dateInput">Date (yyyy-mm-dd)</label><br />
+            <button onClick={addConsumption}>Add consumption of input-field pizza by dropdown person</button>
         </div>
     )
 }
